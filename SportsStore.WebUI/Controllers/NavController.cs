d09096d28663a7e6c3c36bee4e363915f04a0bff
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-23 08:36:32 -0400

    Can_Create_Categories() test method is added

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        public string Menu()
+
+        public NavController(IProductRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
+        }
+
+        private IProductRepository repository;
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                                            .Select(x => x.Category)
+                                            .Distinct()
+                                            .OrderBy(X => X);
+            return PartialView(categories);
         }
     }
 }
