dc03d0182e85a3f8714a3c6edc68788c1b8281f4
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-05 13:55:21 -0500

    updated NavController to work with MenuHorizontal.cshtml view

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -17,7 +17,7 @@ namespace SportsStore.WebUI.Controllers
 
         private IProductRepository repository;
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategory = category;
 
@@ -25,7 +25,8 @@ namespace SportsStore.WebUI.Controllers
                                 .Select(x => x.Category)
                                 .Distinct()
                                 .OrderBy(X => X);
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
         }
     }
 }
